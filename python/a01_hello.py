4d11ac6bb840220ac0941322d1aa6b0bcf4a9d92
sjd <seojeongdae7204@gmail.com>
2026-06-22 15:18:45 +0900

    print

---
+++ python/a01_hello.py
@@ -0,0 +1,8 @@
+import keyword
+
+
+def main():
+    print("hello, world!")
+    print(keyword.kwlist)
+if __name__ == "__main__":
+    main()
---
+++ python/a04_print.py
@@ -0,0 +1,18 @@
+class A_test:
+    def __repr__(self):
+        return "A_test 객체이다."
+
+def main():
+    print(12345)
+    print(123, "seo", "jeong", "dae")
+    print(3.14159)
+    a=A_test()
+    print(a)
+    print("this is", "python", "class", sep="_", end=" ")
+    print("this is", "python", "class", sep="-")
+
+
+
+
+if __name__ == "__main__":
+    main()
