18b2402652efcb741c9741da126cff9fd62f973e
sjd <seojeongdae7204@gmail.com>
2026-06-26 15:17:37 +0900

    ㅋㄴㅁㅇㅁㄴ

---
+++ python/a104_multithreading.py
@@ -0,0 +1,18 @@
+import threading
+import time
+
+
+
+def 
+
+
+def main():
+
+
+
+
+
+
+
+if __name__ == "__main__":
+        main()
---
+++ python/a105_asycio.py
@@ -0,0 +1,19 @@
+import asyncio
+import time
+
+
+
+async def task(name.duratiom):
+    print 
+
+
+def main():
+
+
+
+
+
+
+
+if __name__ == "__main__":
+        main()
